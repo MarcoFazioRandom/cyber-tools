886a0f3b45fc2f0c29c34afba5006767975a2157
Marco F <51234379+MarcoFazioRandom@users.noreply.github.com>
2023-04-10 14:02:50 +0200

    Update scan_tools.py

--- scan_tools.py
+++ scan_tools.py
@@ -2,34 +2,8 @@ from datetime import datetime
 import whois
 import shodan
 import os
-import ports_scanner
 import requests
 import json
-
-COMMON_PORTS = [21, 22, 23, 25, 53, 80, 110, 115, 135, 139, 143, 194, 443, 445, 1433, 3306, 3389, 5632, 5900, 8080, 8443, 25565]
-SHODAN_API_KEY = "KEY"
-ABUSEIPDB_KEY = "KEY"
-
-def ping(target):
-	return not os.system(f"ping {target} -n 1 > NUL")
-	# stream = os.popen(f"ping {target} -n 1")
-	# output = stream.read()
-	# return output
-
-shodan_keys = ["matches", "org", "hostnames", "domains", "C", "country_name", "city", "os", "isp", "last_update"]
-
-def shodan_scan(ip):
-	result = {k:None for k in shodan_keys}
-	api = shodan.Shodan(SHODAN_API_KEY)
-	try:
-		result["matches"] = api.count(ip).get("total", 0)
-		if result["matches"] == 0:
-			return result
-		host = api.host(ip)
-		result = {k:host.get(k, result[k]) for k in shodan_keys}
-	except:
-		pass
-	return result
 
 # dates can be one date or a list of date, this function returns the most recent date.
 def convert_dates(dates):
@@ -42,39 +16,102 @@ def convert_dates(dates):
 		dates = sorted(dates, reverse=True)
 		return dates[0]
 
-whois_keys = ["domain_name", "registrar", "whois_server", "referral_url", "creation_date", "updated_date", "expiration_date", "name_servers", "emails", "org", "country"]
+def ping(target):
+	return not os.system(f"ping {target} -n 1 > NUL")
+	# stream = os.popen(f"ping {target} -n 1")
+	# output = stream.read()
+	# return output
 
-# Whois
-def whois_scan(target):
-	result = {k:None for k in whois_keys}
+_SHODAN_API_KEY = "KEY"
+
+SHODAN_KEYS = ["matches", "org", "hostnames", "domains", "C", "country_name", "city", "os", "isp", "last_update"]
+
+def shodan_check(ip, keys=None):
+	result = {k:None for k in SHODAN_KEYS}
+	api = shodan.Shodan(_SHODAN_API_KEY)
 	try:
-		w = whois.whois(target)
-		result = {k:w.get(k, result[k]) for k in whois_keys}
+		result["matches"] = api.count(ip).get("total", 0)
+		if result["matches"] == 0:
+			return result
+		host = api.host(ip)
+		if keys:
+			result = {k:host.get(k, result[k]) for k in keys}
+		else:
+			result.update(host)
 	except:
 		pass
 	return result
 
-def open_ports_scan(ip):
-	return ports_scanner.port_scan(ip, COMMON_PORTS)
+WHOIS_KEYS = ["domain_name", "registrar", "whois_server", "referral_url", "creation_date", "updated_date", "expiration_date", "name_servers", "emails", "org", "country"]
 
-abuseipdb_keys = ["abuseConfidenceScore", "isWhitelisted", "countryCode", "usageType", "isp", "domain", "hostnames", "totalReports", "lastReportedAt"]
+# Whois
+def whois_check(target):
+	result = {k:None for k in WHOIS_KEYS}
+	try:
+		w = whois.whois(target)
+		result = {k:w.get(k, result[k]) for k in WHOIS_KEYS}
+	except:
+		pass
+	return result
 
-def abuseipdb_scan(ip):
-	result = {k:None for k in abuseipdb_keys}
+_ABUSEIPDB_API_KEY = "KEY"
+
+ABUSEIPDB_KEYS = ["isPublic", "abuseConfidenceScore", "isWhitelisted", "countryCode", "usageType", "isp", "domain", "hostnames", "totalReports", "lastReportedAt"]
+
+def abuseipdb_check(ip, maxDays=180):
+	result = {k:None for k in ABUSEIPDB_KEYS}
+	decodedResponse = ""
 	url = "https://api.abuseipdb.com/api/v2/check"
 	querystring = {
 		"ipAddress": f"{ip}",
-		"maxAgeInDays": "30"
+		"maxAgeInDays": f"{maxDays}", 
+		# "verbose":True
 	}
 	headers = {
 		"Accept": "application/json",
-		"Key": ABUSEIPDB_KEY
+		"Key": _ABUSEIPDB_API_KEY
 	}
+
 	try:
 		response = requests.request(method="GET", url=url, headers=headers, params=querystring)
 		decodedResponse = json.loads(response.text)
-		data = decodedResponse["data"]
-		result = {k:data.get(k, result[k]) for k in abuseipdb_keys}
+
+		if decodedResponse["errors"]:
+			status = decodedResponse["errors"][0]['status']
+			return status
+		
+		data = decodedResponse.get("data")
+		if data:
+			result = {k:data.get(k, result[k]) for k in ABUSEIPDB_KEYS}
 	except:
-		pass
+		return "request error"
+
 	return result
+
+_VIRUSTOTAL_API_KEY = "KEY"
+
+# collectionName can be: files, ip_addresses, urls, domains
+def virustotal_check(collectionName, objectId):
+	url = f"https://www.virustotal.com/api/v3/X{collectionName}/{objectId}"
+	headers = {"x-apikey": _VIRUSTOTAL_API_KEY}
+	response = requests.request(method="GET", url=url, headers=headers)
+	decodedResponse = json.loads(response.text)
+	if decodedResponse.get("error"):
+		return {"error" : decodedResponse.get("error").get("code")}
+	
+	result = {}
+	
+	stats = decodedResponse.get("data").get("attributes").get("last_analysis_stats")
+
+	total_score = stats['malicious'] + stats['suspicious'] + stats['harmless'] + stats['undetected'] + stats['timeout']
+	malicious_score = stats['malicious'] + stats['suspicious']
+	percentage_score = round((malicious_score / total_score) * 100)
+
+	result["vt_score"] = percentage_score
+
+	if collectionName == "files":
+		result["sha256"] = decodedResponse.get("data").get("attributes").get("sha256")
+		result["name"] = decodedResponse.get("data").get("attributes").get("meaningful_name")
+	
+	return result
+
